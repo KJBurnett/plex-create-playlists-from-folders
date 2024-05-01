1b7b18a3a4402ad24631b4b0edc1a1302e56c678
Kyler Burnett <KJBurnett@users.noreply.github.com>
2024-04-30 21:55:18 -0700

    add config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+class Config:
+    def __init__(
+        self,
+        musicLibrary,
+        baseurl,
+        token,
+        playlistFolders,
+    ):
+        self.musicLibrary = musicLibrary
+        self.baseurl = baseurl
+        self.token = token
+        self.playlistFolders = playlistFolders
